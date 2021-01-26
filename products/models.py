08ba27cfe032be0b84573b005fa9531ea2713fe8
Robert Yap <rob@bposeats.com>
2021-01-26 21:30:07 +0800

    Add Product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """A item to be sold.
+
+    The `price` is recorded in Philippine Centavo. So PHP 100.00 is recorded
+    as 10_000. This is to ensure that floating point arithmetic is not used.
+    """
+
+    name = models.CharField(max_length=200)
+    quantity = models.PositiveSmallIntegerField()
+    price = models.IntegerField()
+    discount = models.DecimalField(max_digits=2, decimal_places=2)
+
+    @property
+    def in_stock(self):
+        """Check if a product is in stock."""
+
+        return self.quantity > 0
